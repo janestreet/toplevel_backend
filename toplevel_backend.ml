cd317742cc4981fe5870f56f5524498f5fe7964a
public-release <as-public-release@janestreet.com>
2023-08-30 10:23:17 +0100

    v0.17~preview.128.37+01

--- toplevel_backend.ml
+++ toplevel_backend.ml
@@ -6,9 +6,13 @@ end
 open Compiler_modules
 
 [%%if ocaml_version >= (5, 0, 0)]
+
 let unsafe_string () = true
+
 [%%else]
+
 let unsafe_string () = !Clflags.unsafe_string
+
 [%%endif]
 
 module type S = sig
@@ -18,8 +22,8 @@ end
 
 let init ~native:_ (module M : S) =
   Topfind.log := ignore;
-  Topfind.don't_load_deeply ["toplevel_expect_test"];
-  Topfind.add_predicates ["byte"; "toploop"; "ppx_driver"; "mt"; "mt_posix"];
+  Topfind.don't_load_deeply [ "toplevel_expect_test" ];
+  Topfind.add_predicates [ "byte"; "toploop"; "ppx_driver"; "mt"; "mt_posix" ];
   (* Add findlib path so Topfind is available and it won't be initialized twice if the
      user does [#use "topfind"]. *)
   Topdirs.dir_directory (Findlib.package_directory "findlib")
